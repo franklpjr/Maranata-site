0f87b898859a1b278f0d1fddc9f5d61e4afd077c
franklpjr <eng.civ.frank@gmail.com>
2023-05-30 20:12:57 -0400

    Substituicao do BrowserRouter pelo HashRouter

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import DetailsPage from "../../pages/DetailsPage";
 import ErrorPage from "../../pages/ErrorPage";
 import HomePage from "../../pages/HomePage/HomePage";
@@ -6,13 +6,13 @@ import HomePage from "../../pages/HomePage/HomePage";
 const Router = () => {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route exact path="/Maranata-site/" element={<HomePage />}/>
+                <Route exact path="/" element={<HomePage />}/>
                 <Route exact path="/details/:id" element={<DetailsPage />}/>
                 <Route path="*" element={<ErrorPage />}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
