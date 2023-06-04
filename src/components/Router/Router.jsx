88535269e3f2e11de1dd45033fa2a5dbf46d66cf
franklpjr <eng.civ.frank@gmail.com>
2023-06-04 19:50:57 -0400

    alteracao de BrowserRouter para HashRouter

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import DetailsPage from "../../pages/DetailsPage";
 import ErrorPage from "../../pages/ErrorPage";
 import HomePage from "../../pages/HomePage/HomePage";
@@ -6,13 +6,13 @@ import HomePage from "../../pages/HomePage/HomePage";
 const Router = () => {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/details/:id" element={<DetailsPage />}/>
                 <Route path="*" element={<ErrorPage />}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
