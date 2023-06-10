28d29d91b468d11ca53b42f262ee36866781b0b5
franklpjr <eng.civ.frank@gmail.com>
2023-06-10 19:08:53 -0400

    alteracoes no root

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -3,9 +3,10 @@ import "./Video.css"
 
 function Video() {
 
+    
     return(
         <div className="video-wrapper">
-            <YouTube videoId="-RAo5sK7IdA"/>
+            <YouTube videoId="-RAo5sK7IdA" className='video'/>
         </div>
     )
 }
